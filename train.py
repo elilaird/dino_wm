08df7fc77b0ab5cb0959f30182ed5de2c5c7a53b
elilaird <elilaird3@gmail.com>
2025-09-22 09:53:01 -0500

    testing

--- train.py
+++ train.py
@@ -821,7 +821,7 @@ class Trainer:
             print(f"visuals: {visuals['proprio'].shape}")
             
 
-        for k in z_pred.keys():
+        for k in ['visual']:#z_pred.keys():
             # mse
             logs[f"{k}_horizon_mse"] = torch.nn.functional.mse_loss(z_pred[k], z_tgt[k])
 
